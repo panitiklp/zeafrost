0e5f1fdbdd332fba5bcb50fa213b55ade58c0d32
Paniti Kliengsa-ard <mf8vfqznh890kpi7ca38p96kl6j2sk@bots.bitbucket.org>
2024-10-14 09:44:30 +0700

    mod: implement pipeline config

--- src/sg_entities/sg_pipelineConfiguration.py
+++ src/sg_entities/sg_pipelineConfiguration.py
@@ -11,7 +11,7 @@ SG_FIELD_MAPS = {
     'steps': 'sg_steps',
     'departments': 'sg_departments',
     'rederers': 'sg_rederers',
-    'softwares': 'sg_softwares',
+    'software': 'sg_software',
     'windows_path': 'windows_path',
     'default_shot': 'sg_default_shot',
     'default_asset': 'sg_default_asset'
@@ -24,9 +24,9 @@ STR_FIELDS = ['code']
 #       SHOTGRID ENTITY SEARCH       #
 # ================================== #
 def entity_cache(*args, **kwargs):
-    project         = ''
-    redis_pattern   = ''
-    sg_result       = []
+    project = ''
+    redis_pattern = ''
+    sg_result = []
 
     for arg in [each for each in args if isinstance(each, dict)]:
         for key, val in arg.items():
@@ -40,17 +40,16 @@ def entity_cache(*args, **kwargs):
         sg = sg_con.connect()
         sg_result = sg.find(
             'PipelineConfiguration', 
-            filters =   [['project', 'name_contains', project]],
-            fields  =   list(SG_FIELD_MAPS.values())
+            filters = [['project', 'name_contains', project]],
+            fields = list(SG_FIELD_MAPS.values())
         )
-
         redis_pattern = f'sg:pipelineconfiguration:*{project}:*'.lower()
         redis_ctl.delete(redis_pattern)
 
     for elem in sg_result:
         sg_project = elem.get('project')
         if sg_project:
-            redis_name =  'sg:pipelineconfiguration:'
+            redis_name = 'sg:pipelineconfiguration:'
             redis_name += f'{sg_project.get("name")}:'
             redis_name += f'{elem.get("code")}:'
             redis_name += f'{elem.get("id")}'
@@ -65,12 +64,12 @@ def entity_cache(*args, **kwargs):
 #       SHOTGRID ENTITY SEARCH       #
 # ================================== #
 def sg_entity_search(body):
-    shotgrid    = body.get('shotgrid') or False
-    project     = body.get('project')
-    software    = body.get('software')
-    software_version    = body.get('version')
+    shotgrid = body.get('shotgrid') or False
+    project = body.get('project')
+    software = body.get('software')
+    software_version = body.get('version')
     description = body.get('description')
-    config_id   = body.get('id')
+    config_id = body.get('id')
 
     valid_keys = {
         'shotgrid',
@@ -86,12 +85,12 @@ def sg_entity_search(body):
     
     result = []
 
-    redis_name =  'sg:pipelineconfiguration:'
-    redis_name += f'*{project}:'            if project              else '*:'
-    redis_name += f'*{software}'            if software             else '*'
-    redis_name += f'*{software_version}'    if software_version     else '*'
-    redis_name += f'*{description}'         if description          else '*'
-    redis_name += f'{config_id}'            if config_id            else '*'
+    redis_name = 'sg:pipelineconfiguration:'
+    redis_name += f'*{project}:' if project else '*:'
+    redis_name += f'*{software}' if software  else '*'
+    redis_name += f'*{software_version}' if software_version else '*'
+    redis_name += f'*{description}' if description else '*'
+    redis_name += f'{config_id}' if config_id else '*'
     redis_names = redis_ctl.keys(redis_name.lower())
 
     if len(redis_names) > 0:
@@ -108,8 +107,8 @@ def sg_entity_search(body):
                 result.append(episode_data)
 
     return sg_entity_utils.search_response(
-        data        = result, 
-        shotgrid    = shotgrid, 
-        sg_fields   = SG_FIELD_MAPS_INVERT, 
-        str_fields  = STR_FIELDS
+        data  = result, 
+        shotgrid = shotgrid, 
+        sg_fields = SG_FIELD_MAPS_INVERT, 
+        str_fields = STR_FIELDS
     )
